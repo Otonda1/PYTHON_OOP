4abd1d170184876d805563ebca8621076c45e788
root <root@DESKTOP-ICK7MPV>
2024-05-04 16:23:16 +0300

    introduction of a static method

--- employee.py
+++ employee.py
@@ -23,8 +23,26 @@ class Employee:
     def set_raise_amt(cls, amount):
         cls.raise_amount = amount
 
+    #introduction of a class method as an alternative constructor
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+
+    #introduction of a static method
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+    
+
 #changing the raise amount using the class method
 Employee.set_raise_amt(1.05)
+#using the class method as an alternative constructor
+emp_str_1 = 'john-doe-70000'
+Employee.from_string(emp_str_1)
 
 
 
@@ -32,6 +50,7 @@ print('NUMBER OF EMPLOYEES IS {}'.format(Employee.num_of_emps))
 #initialize instances
 joe = Employee('joe', 'smith', 50000)
 jane = Employee('jane', 'doe', 60000)
-print(jane.raise_amount)
-print(joe.raise_amount)
+
+import datetime
+print(joe.is_workday(datetime.date(2021, 9, 25)))
 
